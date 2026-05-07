0f05b651ebb3a7f01567b4a169c4b44c77989648
mocha <mocha4pf@gmail.com>
2026-05-07 15:23:31 +0300

    created transaction object from form data

--- code.js
+++ code.js
@@ -1,24 +1,34 @@
 const transactionForm = document.getElementById("transactionForm");
 
 transactionForm.addEventListener("submit", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    //capturing user inputs
-    const title = document.getElementById("title").value;
-    const amount = document.getElementById("amount").value;
-    const type = document.getElementById("type").value;
-    const category = document.getElementById("category").value;
-    const date = document.getElementById("date").value;
+  //capturing user inputs
+  const title = document.getElementById("title").value;
+  const amount = document.getElementById("amount").value;
+  const type = document.getElementById("type").value;
+  const category = document.getElementById("category").value;
+  const date = document.getElementById("date").value;
 
-    //form validation
-    if (
-      title === "" ||
-      amount === "" ||
-      type === "" ||
-      category === "" ||
-      date === ""
-    ) {
-      alert("Please fill in all fields");
-      return;
-    }
+  //form validation
+  if (
+    title === "" ||
+    amount === "" ||
+    type === "" ||
+    category === "" ||
+    date === ""
+  ) {
+    alert("Please fill in all fields");
+    return;
+  }
+  //saving the transactions as objects
+  const transaction = {
+    title: title,
+    amount: amount,
+    type: type,
+    category: category,
+    date: date,
+  };
+
+  console.log(transaction);
 });
