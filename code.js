49bc040b4988981043406143931ca107a7637b1d
mocha <mocha4pf@gmail.com>
2026-05-07 19:01:31 +0300

    implemented transaction saving and display functionality

--- code.js
+++ code.js
@@ -1,71 +1,102 @@
+console.log("code.js is connected");
 const transactionForm = document.getElementById("transactionForm");
-//initialize transactions as an array
+
+// initialize transactions as an array
 let transactions = [];
 
-//checks for saved transactions and pushes them into array
+// checks for saved transactions and pushes them into array
 const savedTransactions = localStorage.getItem("transactions");
 
 if (savedTransactions) {
   transactions = JSON.parse(savedTransactions);
 }
-//form handler
-transactionForm.addEventListener("submit", function (event) {
-  event.preventDefault();
 
-  //capturing user inputs
-  const title = document.getElementById("title").value;
-  const amount = document.getElementById("amount").value;
-  const type = document.getElementById("type").value;
-  const category = document.getElementById("category").value;
-  const date = document.getElementById("date").value;
+// form handler
+if (transactionForm) {
+  transactionForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  //form validation
-  if (
-    title === "" ||
-    amount === "" ||
-    type === "" ||
-    category === "" ||
-    date === ""
-  ) {
-    alert("Please fill in all fields");
-    return;
-  }
-  //saving the transactions as objects
-  const transaction = {
-    title: title,
-    amount: amount,
-    type: type,
-    category: category,
-    date: date,
-  };
-  //add the transaction object inside the transactions array
-  transactions.push(transaction);
-  //save the array in Local storage
-  localStorage.setItem("transactions", JSON.stringify(transactions));
-  //resets form after saving transaction
-  transactionForm.reset();
-  //alerts user
-  alert("Transaction added successfully");
-});
+    // capturing user inputs
+    const title = document.getElementById("title").value;
+    const amount = document.getElementById("amount").value;
+    const type = document.getElementById("type").value;
+    const category = document.getElementById("category").value;
+    const date = document.getElementById("date").value;
 
-//grab table body element
+    // form validation
+    if (
+      title === "" ||
+      amount === "" ||
+      type === "" ||
+      category === "" ||
+      date === ""
+    ) {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    // saving the transaction as an object
+    const transaction = {
+      id: Date.now(),
+      title: title,
+      amount: amount,
+      type: type,
+      category: category,
+      date: date,
+    };
+
+    // add transaction object inside the transactions array
+    transactions.push(transaction);
+
+    // save the array in localStorage
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    // resets form after saving transaction
+    transactionForm.reset();
+
+    // alerts user
+    alert("Transaction added successfully");
+  });
+}
+
+// grab table body element
 const transactionsContainer = document.getElementById("transactionsContainer");
-
 
 if (transactionsContainer) {
   transactions.forEach(function (transaction) {
     const row = document.createElement("tr");
 
     row.innerHTML = `
-    <td>${transaction.title}</td>
-    <td>${transaction.amount}</td>
-    <td>${transaction.type}</td>
-    <td>${transaction.category}</td>
-    <td>${transaction.date}</td>
-`;
+      <td>${transaction.title}</td>
+      <td>${transaction.amount}</td>
+      <td>${transaction.type}</td>
+      <td>${transaction.category}</td>
+      <td>${transaction.date}</td>
 
-transactionsContainer.appendChild(row);
+      <td>
+        <button class= "delete-btn" data-id= "${transaction.id}">
+          Delete
+        </button>
+      </td>
+    `;
 
+    transactionsContainer.appendChild(row);
+    //adding event listener on delete button
+    const deleteButton = row.querySelector(".delete-btn");
 
+    deleteButton.addEventListener("click", function () {
+      const transactionId = Number(deleteButton.dataset.id);
+
+      deleteTransaction(transactionId);
+      location.reload;
+    });
   });
+}
+//delete function
+function deleteTransaction(id) {
+  transactions = transactions.filter(function (transaction) {
+    return transaction.id !== id;
+  });
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
