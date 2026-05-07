ca8be72589ec88bf7f5d1589c4172e3aeba871df
mocha <mocha4pf@gmail.com>
2026-05-07 15:20:42 +0300

    added form validation for empty fields

--- code.js
+++ code.js
@@ -3,9 +3,22 @@ const transactionForm = document.getElementById("transactionForm");
 transactionForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    //capturing user inputs
     const title = document.getElementById("title").value;
     const amount = document.getElementById("amount").value;
     const type = document.getElementById("type").value;
     const category = document.getElementById("category").value;
     const date = document.getElementById("date").value;
+
+    //form validation
+    if (
+      title === "" ||
+      amount === "" ||
+      type === "" ||
+      category === "" ||
+      date === ""
+    ) {
+      alert("Please fill in all fields");
+      return;
+    }
 });
